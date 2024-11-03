694ce5c76610dbde9180d0f0b81ff42e4ee87ab1
jb <jeanb.rocher@gmail.com>
2024-11-03 17:45:39 +0100

    feat: intialiaze the real estate module

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Real Estate",
+    "version": "0.7.0",
+    "summary": "Manage all your real estates needs",
+    "description": "",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    "css": [],
+    "license": "LGPL-3",
+    "application": True,
+}
---
+++ real_estate/models/estate_property.py
@@ -0,0 +1,28 @@
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "real.estate.property"
+    _description = "A real estate property"
+
+    name = fields.Char(string="Name")
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Date Availability")
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        string="Garden Orientation",
+    )
